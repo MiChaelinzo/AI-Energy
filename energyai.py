64ef1ee4180646b1a88e14893798172de8104b05
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-06-21 18:56:50 +0300

    Create energyai.py

---
+++ energyai.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import geopandas as gpd
+
+# Load satellite data using WEkEO data/service
+wind_farm_data = gpd.read_file('wind_farm_data.geojson')  # Replace 'wind_farm_data.geojson' with the actual wind farm data file
+bird_migration_data = gpd.read_file('bird_migration_data.geojson')  # Replace 'bird_migration_data.geojson' with the actual bird migration data file
+solar_panel_data = gpd.read_file('solar_panel_data.geojson')  # Replace 'solar_panel_data.geojson' with the actual solar panel data file
+
+# Perform analysis on wind farms and bird migration patterns
+# ...
+
+# Example analysis: Assess proximity of wind farms to bird migration routes
+def assess_proximity(wind_farm_data, bird_migration_data):
+    # Perform spatial join to determine which bird migration routes intersect with wind farms
+    bird_migration_intersect = gpd.sjoin(bird_migration_data, wind_farm_data, op='intersects')
+
+    # Calculate the distance between bird migration routes and wind farms
+    bird_migration_intersect['distance'] = bird_migration_intersect.geometry.distance(bird_migration_intersect.geometry)
+
+    # Identify bird migration routes that are in close proximity to wind farms
+    close_routes = bird_migration_intersect[bird_migration_intersect['distance'] < 1000]  # Adjust the threshold as needed
+
+    # Print the close bird migration routes and associated wind farms
+    for index, row in close_routes.iterrows():
+        print(f"Bird Migration Route {row['route_id']} is in proximity to Wind Farm {row['wind_farm_id']}.")
+
+# Call the function to assess proximity of wind farms to bird migration routes
+assess_proximity(wind_farm_data, bird_migration_data)
+
+# Perform analysis on solar panels and local ecosystems
+# ...
+
+# Example analysis: Identify areas of high solar panel density in sensitive ecosystems
+def identify_sensitive_areas(solar_panel_data, local_ecosystem_data):
+    # Perform spatial join to determine which local ecosystems intersect with solar panels
+    ecosystems_intersect = gpd.sjoin(local_ecosystem_data, solar_panel_data, op='intersects')
+
+    # Calculate the solar panel density within each ecosystem
+    ecosystems_intersect['density'] = ecosystems_intersect.groupby('ecosystem_id')['solar_panel_id'].transform('count')
+
+    # Identify ecosystems with high solar panel density
+    high_density_ecosystems = ecosystems_intersect[ecosystems_intersect['density'] > 10]  # Adjust the threshold as needed
+
+    # Print the high-density ecosystems and associated solar panels
+    for index, row in high_density_ecosystems.iterrows():
+        print(f"Sensitive Ecosystem {row['ecosystem_id']} has high solar panel density.")
+
+# Call the function to identify sensitive areas with high solar panel density
+identify_sensitive_areas(solar_panel_data, local_ecosystem_data)
+
+# Example AI-based analysis: Predict the impact of wind farms on bird migration
+def predict_impact(wind_farm_data, bird_migration_data):
+    # Extract features from wind farm and bird migration data
+    wind_farm_features = wind_farm_data[['feature1', 'feature2', ...]]  # Replace 'feature1', 'feature2', ... with actual relevant features
+    bird_migration_features = bird_migration_data[['feature1', 'feature2', ...]]  # Replace 'feature1', 'feature2', ... with actual relevant features
+
+    # Prepare the dataset for training
+    X = pd.concat([wind_farm_features, bird_migration_features], axis=1)
+    y = wind_farm_data['impact_label']  # Replace 'impact_label' with the actual impact label column
+
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train a random forest classifier
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    # Make predictions on the testing set
+    y_pred = model.predict(X_test)
+
+    # Evaluate the model
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+    # Use the trained model for prediction on new data
+    new_data = pd.concat([wind_farm_features, bird_migration_features], axis=1)
+    predictions = model.predict(new_data)
+
+    # Return the predictions
+    return predictions
+
+# Call the function to predict the impact of wind farms on bird migration
+predicted_impact = predict_impact(wind_farm_data, bird_migration_data)
+
+# Further analysis or decision-making based on the predictions
+def analyze_predictions(wind_farm_data, bird_migration_data, predicted_impact):
+    # Merge wind farm and bird migration data with the predicted impact
+    merged_data = pd.concat([wind_farm_data, bird_migration_data], axis=1)
+    merged_data['predicted_impact'] = predicted_impact
+
+    # Analyze the predicted impact
+    impact_counts = merged_data['predicted_impact'].value_counts()
+    print("Predicted Impact Counts:")
+    print(impact_counts)
+
+    # Calculate the percentage of wind farms with different impact levels
+    impact_percentage = impact_counts / len(merged_data) * 100
+    print("Predicted Impact Percentage:")
+    print(impact_percentage)
+
+    # Further decision-making based on the predicted impact
+    # For example, you can filter the wind farms based on the predicted impact levels and their geographical location to identify areas with potentially higher or lower impact on bird migration.
+
+# Call the function to analyze the predictions and make further decisions
+analyze_predictions(wind_farm_data, bird_migration_data, predicted_impact)
